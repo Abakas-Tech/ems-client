408e30d9446ce36bd4db797e149550f74444d2ec
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-08-16 14:23:00 +0300

    Add Featured component

--- src/pages/public/landingpage.jsx
+++ src/pages/public/landingpage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
+import Featured from "../../components/Featured/Featured";
 
 function landingpage() {
   return (
     <>
       <Hero />
+      <Featured />
     </>
   );
 }
