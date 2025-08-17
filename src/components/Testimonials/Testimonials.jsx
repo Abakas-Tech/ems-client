a8c2bcde719e719e2246f698a85945c1b6d9f4ea
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-08-17 14:57:04 +0300

    update descritpion and images

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import image1 from "./img/image-1.png";
+import image2 from "./img/image-2.png";
+import image3 from "./img/image-3.png";
 
 const testimonialData = [
   {
     name: "Adam Williams",
     position: "CEO Of Microwoft",
-    image: "https://placehold.co/500x500",
+    image: { image1 },
     quote:
       "Cicero famously orated against his political opponent Lucius Sergius Catilina. Occasionally the first Oration against Catiline is taken specimens.",
     color: "bg-main",
@@ -16,7 +18,7 @@ const testimonialData = [
   {
     name: "Retha Deowalim",
     position: "CEO Of Apple",
-    image: "https://placehold.co/500x500",
+    image: { image2 },
     quote:
       "Cicero famously orated against his political opponent Lucius Sergius Catilina. Occasionally the first Oration against Catiline is taken specimens.",
     color: "bg-green",
@@ -24,7 +26,7 @@ const testimonialData = [
   {
     name: "Sam J. Wasim",
     position: "Pio Founder",
-    image: "https://placehold.co/500x500",
+    image: { image3 },
     quote:
       "Cicero famously orated against his political opponent Lucius Sergius Catilina. Occasionally the first Oration against Catiline is taken specimens.",
     color: "bg-red",
@@ -32,7 +34,7 @@ const testimonialData = [
   {
     name: "Usan Gulwarm",
     position: "CEO Of Facewarm",
-    image: "https://placehold.co/500x500",
+    image: { image1 },
     quote:
       "Cicero famously orated against his political opponent Lucius Sergius Catilina. Occasionally the first Oration against Catiline is taken specimens.",
     color: "bg-primary",
@@ -40,7 +42,7 @@ const testimonialData = [
   {
     name: "Shilpa Shethy",
     position: "CEO Of Zapple",
-    image: "https://placehold.co/500x500",
+    image: { image3 },
     quote:
       "Cicero famously orated against his political opponent Lucius Sergius Catilina. Occasionally the first Oration against Catiline is taken specimens.",
     color: "bg-warning",
@@ -52,20 +54,20 @@ const Testimonials = () => {
     dots: false,
     infinite: true,
     speed: 600,
-    slidesToShow: 3, 
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
-    arrows: false, 
+    arrows: false,
     responsive: [
       {
-        breakpoint: 992, 
+        breakpoint: 992,
         settings: {
           slidesToShow: 2,
         },
       },
       {
-        breakpoint: 768, 
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
         },
@@ -81,9 +83,9 @@ const Testimonials = () => {
             <div className="sec-heading center">
               <h2>Good Reviews by Customers</h2>
               <p>
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                blanditiis praesentium voluptatum deleniti atque corrupti quos
-                dolores
+                Hear from my satisfied clients who found their dream homes and
+                profitable investments with me. Their trust is my greatest
+                reward.
               </p>
             </div>
           </div>
