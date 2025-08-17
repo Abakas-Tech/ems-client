9c4723290930c82e6e019591669c0880009cbe43
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-08-17 14:48:43 +0300

    Add Testimonial section to homepage

--- src/pages/public/landingpage.jsx
+++ src/pages/public/landingpage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Featured from "../../components/Featured/Featured";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 function landingpage() {
   return (
     <>
       <Hero />
       <Featured />
+      <Testimonials />
     </>
   );
 }
