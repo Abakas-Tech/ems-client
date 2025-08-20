4a1df693f6a74b0eb5aa16785f9aaf5482c3170c
Ekram Kedir <wasungncom@gmail.com>
2025-08-20 22:17:06 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+
 import "./App.css";
-import PropertyListPage from "./components/properties/properties.list";
-import ChatBot from "./components/Chat/ChatBot";
-import AboutSnippet from './components/AboutSnippet/AboutSnippet';
-import PropertyList from "./components/properties/properties.list";
+
 
 function App() {
   return (
-   <>test</>
+ <></>
   );
 }
 
