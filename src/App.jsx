35321d8fcf62f8c0a2d690c364b595af0ca92a62
Feysel Ansar <ansarfeysel@gmail.com>
2025-08-21 16:24:08 +0300

    update

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-import AppRouter from "./router/router"; 
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
--- src/components/Loader/Loader.jsx
+++
@@ -1,6 +0,0 @@
-import FadeLoader from "react-spinners/ClipLoader";
-
-function Loader() {
-  return <FadeLoader color="#2c3e50" size={50} />;
-}
-export default Loader;
