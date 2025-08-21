6a73622d3428067316224187b1062ccbf9b8571d
Ekram Kedir <wasungncom@gmail.com>
2025-08-21 23:13:22 +0300

    global componts renderd in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
-import AppRouter from "./router/router"; 
+import Loader from "./components/global/Loader/Loader";
+import Logout from "./components/global/Logout/Logout";
+import Response from "./components/global/Response/Response";
+import AppRouter from "./router/router";
 function App() {
-  return <AppRouter />;
+  return (
+    <div>
+      <AppRouter />
+      {/* Global components */}
+      <Logout />
+      <Response />
+      <Loader />
+    </div>
+  );
 }
 
 export default App;
