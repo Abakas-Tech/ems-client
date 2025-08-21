a3875984a70f9e170e49c668a41f728620bfd1be
Feysel Ansar <ansarfeysel@gmail.com>
2025-08-21 06:34:38 +0300

    Merge with updated dev

--- src/api/public/about.api.jsx
+++ src/api/public/about.api.jsx
@@ -1,15 +1,12 @@
-import axiosInstance from "../../utils/axios";
+import { axiosInstance } from "../../utils/axios";
 
 // Fetch agent profile
 export const fetchAgentProfile = async () => {
   try {
     const response = await axiosInstance.get("/agent-profile");
-    console.log("Agent response:", response);
     return response.data.data || null; // return agent object
   } catch (error) {
     console.error("Error fetching agent profile:", error.message);
     return null;
   }
 };
-
-
--- src/api/public/contact.api.jsx
+++ src/api/public/contact.api.jsx
@@ -1,5 +1,4 @@
-
-import axiosInstance from "../../utils/axios";
+import { axiosInstance } from "../../utils/axios";
 export const sendContactMessage = async (contactForm) => {
   const res = await axiosInstance.post("/contact", contactForm);
   return res.data.data;
