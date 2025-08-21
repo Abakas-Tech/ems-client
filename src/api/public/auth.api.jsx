50440be2ddac7d89012085d99e8ec8d626bc28b4
Ekram Kedir <wasungncom@gmail.com>
2025-08-21 22:10:07 +0300

    check auth api removed

--- src/api/public/auth.api.jsx
+++
@@ -1,7 +0,0 @@
-import {axiosInstance} from "../../utils/axios";
-
-// Check current logged-in user
-export const checkUser = async () => {
-  return await axiosInstance.get("api/check/user");
-};
-
