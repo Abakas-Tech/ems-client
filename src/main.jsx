551aa75378daf976c515c3ba73e8842246c20f21
Ekram Kedir <wasungncom@gmail.com>
2025-08-21 23:16:35 +0300

    app wrapped with context providers

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,23 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
+import LoaderProvider from "./context/Loader/LoaderProvider";
+import LogoutProvider from "./context/logout/LogoutProvider.jsx";
+import ResponseProvider from "./context/response/ResponseProvider.jsx";
+import { AuthProvider } from "./context/auth/authProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <LoaderProvider>
+          <LogoutProvider>
+            <ResponseProvider>
+              <App />
+            </ResponseProvider>
+          </LogoutProvider>
+        </LoaderProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
