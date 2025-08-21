611006774e5bf956d2378b57c35137d6f9f9f23d
Ekram Kedir <wasungncom@gmail.com>
2025-08-21 22:55:38 +0300

    logot confirm added

---
+++ src/components/global/Logout/Logout.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import styles from "./LogoutConfirmModal.module.css";
+
+const Logout = ({ show, onClose, onConfirm }) => {
+  return (
+    <Modal show={show} onHide={onClose} centered backdrop="static">
+      <div className={styles.modalContent}>
+        <Modal.Body>
+          <h5 className={styles.title}>Are you sure you want to logout?</h5>
+          <div className={styles.actions}>
+            <Button
+              variant="secondary"
+              onClick={onClose}
+              className={styles.cancelBtn}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="danger"
+              onClick={onConfirm}
+              className={styles.logoutBtn}
+            >
+              Logout
+            </Button>
+          </div>
+        </Modal.Body>
+      </div>
+    </Modal>
+  );
+};
+
+export default Logout;
