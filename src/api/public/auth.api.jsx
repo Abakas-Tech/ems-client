25c8501b29f3cb39a3d27f1916da84ee7b8fe889
Ekram Kedir <wasungncom@gmail.com>
2025-08-21 22:00:02 +0300

    checkUser api added

---
+++ src/api/public/auth.api.jsx
@@ -0,0 +1,7 @@
+import {axiosInstance} from "../../utils/axios";
+
+// Check current logged-in user
+export const checkUser = async () => {
+  return await axiosInstance.get("api/check/user");
+};
+
