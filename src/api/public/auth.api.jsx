f1bf3bbbbd9c60c14cd111462ac3003d2f29c2f9
Ekram Kedir <wasungncom@gmail.com>
2025-08-21 23:27:24 +0300

    auth token attached in header

--- src/api/public/auth.api.jsx
+++ src/api/public/auth.api.jsx
@@ -1,7 +1,11 @@
 import {axiosInstance} from "../../utils/axios";
 
-// Check current logged-in user
+//  Check current logged-in user with Bearer token
 export const checkUser = async () => {
-  return await axiosInstance.get("api/auth/check/user");
+  const token = localStorage.getItem("authToken");
+  return await axiosInstance.get("api/auth/check-user", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
-
