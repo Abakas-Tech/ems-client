ca3dba40598c3da297f78c5c81b4466e3f273a33
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-08-27 16:49:11 +0300

    Update the Logout modal css

--- src/context/logout/LogoutProvider.jsx
+++ src/context/logout/LogoutProvider.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { LogoutContext } from "./LogoutContext.jsx";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../auth/UseAuth.jsx";
-import Logout from './../../components/global/Logout/Logout';
+import Logout from "./../../components/global/Logout/Logout";
 
 const LogoutProvider = ({ children }) => {
   const navigate = useNavigate();
