3867f32b8a09b481d3e99e175369d75e696ea256
Feysel Ansar <ansarfeysel@gmail.com>
2025-08-29 17:08:27 +0300

    update the folder structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import Loader from "./shared/global/Loader/Loader";
-import Logout from "./shared/global/Logout/Logout";
-import Response from "./shared/global/Response/Response";
 import AppRouter from "./routes/router";
 function App() {
   return (
-    <div>
+    <>
       <AppRouter />
-      {/* Global components */}
-      <Logout />
-      <Response />
-      <Loader />
-    </div>
+    </>
   );
 }
 
