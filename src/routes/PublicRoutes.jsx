28919a091aaaf7a63f4674061c2f367161b212f6
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-03-15 14:22:27 +0300

    add public routes

---
+++ src/routes/PublicRoutes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import MainLayout from "../shared/layout/MainLayout/MainLayout";
+import AboutDetail from "../domains/public/pages/About/About";
+import NotFound from "../shared/pages/NotFound/NotFound";
+import LandingPage from "../domains/public/pages/Landing/landingpage";
+
+function PublicRoutes() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about-detial" element={<AboutDetail />} />
+      </Route>
+
+      {/* fallback route */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default PublicRoutes;
