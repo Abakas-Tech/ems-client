ec21fccbc9f91c22a30a8d1c918396a01dc911df
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-03-23 23:07:26 +0300

    enhace hero styles

--- src/domains/public/components/Hero/Hero.jsx
+++ src/domains/public/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import banner from "../../../../assets/img/banner/banner.png";
 import styles from "./Hero.module.css";
 
 function Hero() {
@@ -9,12 +8,6 @@ function Hero() {
         <div className="row align-items-center">
           <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12">
             <div className="d-flex align-items-center justify-content-start mb-2">
-              <div className="label rounded-pill bg-white text-dark d-flex align-items-center justify-content-center px-2 py-2 pe-3">
-                <span className="label bg-green rounded-pill text-uppercase me-2">
-                  New
-                </span>
-                Get 20% Off with Super Agent
-              </div>
             </div>
           </div>
         </div>
