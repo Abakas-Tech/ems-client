14259591ad99fc54d845181b9d167ff0d414081c
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-03-26 21:11:41 +0300

    add partner router component and define it's routes

--- src/routes/PartnerRoutes.jsx
+++ src/routes/PartnerRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import MyApplication from "../domains/admin/pages/workers/WorkerProfile/WorkerProfile.jsx";
 import MyProfile from "../domains/admin/pages/Profile/Profile.jsx";
 import MyNotifications from "../domains/admin/pages/NotificationPage/NotificationPage.jsx";
 import NotFound from "../shared/components/NotFound/NotFound.jsx";
 import AdminLayout from "../shared/layout/AdminLayout/AdminLayout.jsx";
 import ProtectedRoute from "../utils/ProtectedRoute.jsx";
+import ActiveWorkers from "../domains/admin/pages/workers/ActiveWorkers/ActiveWorkers.jsx";
+import File from "../domains/admin/pages/FileManager/FileManager.jsx";
+import ChangePasswordPage from "../domains/admin/pages/ChangePassword/ChangePassword.jsx";
 
 function WorkerRoutes() {
   return (
@@ -17,9 +19,11 @@ function WorkerRoutes() {
           </ProtectedRoute>
         }
       >
-        <Route path="my-application/:id" element={<MyApplication />} />
+        <Route path="workers" element={<ActiveWorkers />} />
+        <Route path="my-files" element={<File />} />
         <Route path="my-profile" element={<MyProfile />} />
         <Route path="notifications" element={<MyNotifications />} />
+        <Route path="settings" element={<ChangePasswordPage />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
