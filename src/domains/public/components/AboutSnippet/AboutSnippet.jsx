8e55e8c2a8d586a981b699125e147a0e80764836
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-03-28 10:32:28 +0300

    change header logo, banner image, and use real logo for about snippet

--- src/domains/public/components/AboutSnippet/AboutSnippet.jsx
+++ src/domains/public/components/AboutSnippet/AboutSnippet.jsx
@@ -1,5 +1,5 @@
 import { FaArrowRight } from "react-icons/fa";
-import about from "../../../../assets/img/Banner/banner.jpg";
+import about from "../../../../assets/img/logo/logo.jpg";
 import { useNavigate } from "react-router-dom";
 
 function About() {
--- src/domains/public/components/Hero/Hero.jsx
+++ src/domains/public/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import banner from "../../../../assets/img/banner/banner.png";
 import styles from "./Hero.module.css";
 
 function Hero() {
@@ -8,20 +7,7 @@ function Hero() {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12">
-            <div className="d-flex align-items-center justify-content-start mb-2">
-              <div className="label rounded-pill bg-white text-dark d-flex align-items-center justify-content-center px-2 py-2 pe-3">
-                <span className="label bg-green rounded-pill text-uppercase me-2">
-                  New
-                </span>
-                Get 20% Off with Super Agent
-              </div>
-            </div>
-
-            <h1 className="text-white">
-              Find Your Dream House
-              <br />
-              {/* In <span className="text-primary">{text}|</span> */}
-            </h1>
+            <div className="d-flex align-items-center justify-content-start mb-2"></div>
           </div>
         </div>
       </div>
