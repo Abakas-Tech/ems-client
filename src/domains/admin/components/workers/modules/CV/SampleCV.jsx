921487e418c7f366e25907ce1561ee103894a598
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-06-09 11:11:35 +0300

    edit column names to match sample cv, add top margint to the table

--- src/domains/admin/components/workers/modules/CV/SampleCV.jsx
+++ src/domains/admin/components/workers/modules/CV/SampleCV.jsx
@@ -5,11 +5,11 @@ function SampleCV() {
   return (
     <>
       <img src={cvHeader} alt="CV Header" className="w-full h-auto img-fluid" />
-      <table className="table table-bordered">
+      <table className="table table-bordered mt-3">
         <thead>
           <tr>
             <th colSpan="3" className="text-center fw-bold">
-              Application for Employment التوظيف
+              Application for Employment طلب التوظيف 
             </th>
 
             {/* empty header cell for photo column */}
@@ -21,7 +21,7 @@ function SampleCV() {
           <tr>
             <td>Reference No</td>
             <td>Ahmed</td>
-            <td>الاسم الأول</td>
+            <td>رقم المرجع</td>
 
             <td rowSpan="5" className="text-center align-middle">
               <img
@@ -39,19 +39,19 @@ function SampleCV() {
           <tr>
             <td>Post Applied For</td>
             <td>Ali</td>
-            <td>اسم الأب</td>
+            <td>وظيفة</td>
           </tr>
 
           <tr>
             <td>Monthly Salary</td>
             <td>Hassan</td>
-            <td>اسم العائلة</td>
+            <td>راتب العاقد</td>
           </tr>
 
           <tr>
             <td>Contract Period</td>
             <td>P1234567</td>
-            <td>رقم الجواز</td>
+            <td>مدة العقد</td>
           </tr>
 
          
