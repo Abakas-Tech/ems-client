3b6299200b07dec633a0a01dfad340a4b7abb6ab
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-06-29 11:51:34 +0300

    add file search page, and menu config

---
+++ src/domains/admin/pages/FileSearch/FileSearch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FileSearchComponent from "../../components/files/FileSearch/FileSearch";
+function FileSearch() {
+  return (
+    <>
+      <FileSearchComponent />
+    </>
+  );
+}
+
+export default FileSearch;
