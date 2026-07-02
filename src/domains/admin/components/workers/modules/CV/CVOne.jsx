0253129a74037a8669e14b396ade6ae26ebff094
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-07-02 13:45:48 +0300

    update cv two style, add experience row for cv one

--- src/domains/admin/components/workers/modules/CV/CVOne.jsx
+++ src/domains/admin/components/workers/modules/CV/CVOne.jsx
@@ -274,10 +274,14 @@ const CVOne = ({ templateSwitcher }) => {
   const lang =
     worker.languages?.map((l) => l.language ?? l.name).join(", ") ?? "";
   const edu = (worker.education ?? "").toUpperCase();
-  const expP = worker.experience?.[0]?.years
-    ? `${worker.experience[0].years} yrs`
+  const expP = worker.experience?.length
+    ? worker.experience
+        .map((e) => (e.years ? `${e.years} yrs` : ""))
+        .join(" / ")
     : "";
-  const expC = worker.experience?.[0]?.country ?? "";
+  const expC = worker.experience?.length
+    ? worker.experience.map((e) => e.country ?? "").join(" / ")
+    : "";
   const ppNo = worker.passport_number ?? "";
   const ppIssue = safeDate(worker.passport_issue_date);
   const ppPlace = worker.passport_issuing_country ?? "";
--- src/domains/admin/components/workers/modules/CV/CVTwo.jsx
+++ src/domains/admin/components/workers/modules/CV/CVTwo.jsx
@@ -1020,7 +1020,7 @@ const CVTwo = ({ templateSwitcher }) => {
                           <table>
                             <colgroup>
                               <col style={{ width: "40%" }} />
-                              <col style={{ width: "40%" }} />
+                              <col style={{ width: "30%" }} />
                               <col style={{ width: "40%" }} />
                             </colgroup>
                             <tbody>
