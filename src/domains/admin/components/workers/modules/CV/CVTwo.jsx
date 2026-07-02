4cedca5bb00653c4d9e731e55d46848c1d12b7f8
Feysel Ansar <ansarfeysel@gmail.com>
2026-07-02 11:23:03 +0300

    Fix the title and description of cv

--- src/domains/admin/components/workers/modules/CV/CVTwo.jsx
+++ src/domains/admin/components/workers/modules/CV/CVTwo.jsx
@@ -284,8 +284,8 @@ const CVTwo = ({ templateSwitcher }) => {
     <div className="dashboard-wraper">
       <div className="d-flex flex-column flex-md-row justify-content-between align-items-md-start gap-3">
         <div className="mt-0">
-          <h2 className="fw-bold text-dark mb-2">Country</h2>
-          <p className="text-muted mb-0">Manage countries — create, rename,</p>
+          <h2 className="fw-bold text-dark mb-2">CV</h2>
+          <p className="text-muted mb-0">Generate and upload CV</p>
         </div>
 
         <div className="position-absolute top-0 end-0 mt-4 pt-2">
