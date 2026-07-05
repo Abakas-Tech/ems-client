047c09372f370b2819795d21523142e79e706f66
Abdulwasie <wasungncom@gmail.com>
2026-07-06 00:56:46 +0300

    postion updated

--- src/domains/admin/components/Insurance/InsuranceReportTemplate.jsx
+++ src/domains/admin/components/Insurance/InsuranceReportTemplate.jsx
@@ -130,9 +130,10 @@ const buildDocumentHtml = (doc, index, total, logos) => {
     <div class="beneficiaryRow totalRow">
       <span class="beneficiaryIndex"></span>
       <span class="beneficiaryCell colName"></span>
+        <span class="beneficiaryCell colAddr"></span>
       <span class="beneficiaryCell colRel totalLabel">Total</span>
-      <span class="beneficiaryCell colPct center"><span class="line totalValue">100%</span></span>
-      <span class="beneficiaryCell colAddr"></span>
+      <span class="beneficiaryCell colPct center"><span class="totalValue">100%</span></span>
+    
     </div>
 
     <div class="attachNote">Please attach a copy of Passport and Kebele ID to this form.</div>
