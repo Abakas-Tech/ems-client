8ecb55a8d2dbb52f6a66aa62fd8dd94faf79078c
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:21:45 +0300

    chore(cleanup): remove EMS worker module pages

--- src/domains/admin/pages/workers/modules/Lmis/Lmis.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LmisComponent from './../../../../components/workers/modules/Lmis/Lmis';
-
-export default function Lmis() {
-  return (
-   <LmisComponent/>
-  )
-}
