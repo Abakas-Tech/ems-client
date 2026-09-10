39b896d459ea690d1d5631c7f26f3abf910793bb
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:23:37 +0300

    refactor(routes): drop worker/partner/employer portal routes (EMS-specific)

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -6,10 +6,6 @@ import ScrollToTop from "../shared/components/ScrollToTop/ScrollToTop.jsx";
 import NotFound from "../shared/components/NotFound/NotFound.jsx";
 import AdminRoutes from "./AdminRoutes.jsx";
 import AuthRoutes from "./AuthRoutes.jsx";
-import WorkerRoutes from "./WorkerRoutes.jsx";
-import PartnerRoutes from "./PartnerRoutes.jsx";
-import EmployerRoutes from "./EmployerRoutes.jsx";
-
 
 function AppRouter() {
   return (
@@ -18,16 +14,12 @@ function AppRouter() {
 
       {/* Global Toaster */}
       <Toaster position="top-right" reverseOrder={false} />
-    
 
       <Routes>
         {/* Auth Routes */}
         <Route path="/*" element={<AuthRoutes />} />
         {/* Protected Routes */}
         <Route path="/admin/*" element={<AdminRoutes />} />
-        <Route path="/employee/*" element={<WorkerRoutes />} />
-        <Route path="/partner/*" element={<PartnerRoutes />} />
-        <Route path="/employer/*" element={<EmployerRoutes />} />
 
         {/* 404 - Always Last */}
         <Route path="*" element={<NotFound />} />
