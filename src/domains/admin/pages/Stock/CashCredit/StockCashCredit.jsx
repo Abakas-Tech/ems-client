0cb00cfb3e0e5e75d033ceee25fbf1373c788786
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 09:57:11 +0300

    feat(stock): add cash and credit page

---
+++ src/domains/admin/pages/Stock/CashCredit/StockCashCredit.jsx
@@ -0,0 +1,5 @@
+import StockCashCreditComponent from "../../../components/Stock/CashCredit/StockCashCredit";
+
+export default function StockCashCredit() {
+  return <StockCashCreditComponent />;
+}
