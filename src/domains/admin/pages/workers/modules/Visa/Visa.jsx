89d024b34df69f1168eb2eadc2840f84606d87c7
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:21:53 +0300

    chore(cleanup): remove EMS worker module pages

--- src/domains/admin/pages/workers/modules/Visa/Visa.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import VisaComponent from './../../../../components/workers/modules/Visa/Visa';
-
-export default function Visa() {
-  return (
-    <VisaComponent/>
-  )
-}
