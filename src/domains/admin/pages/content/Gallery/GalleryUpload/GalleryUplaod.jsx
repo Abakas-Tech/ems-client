549d0aafaa47df37b88dcd763683ba0f64128253
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:22:22 +0300

    chore(cleanup): remove EMS content pages

--- src/domains/admin/pages/content/Gallery/GalleryUpload/GalleryUplaod.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import GalleryUploadComponent from '../../../../components/content/Gallery/GalleryUpload/GalleryUpload'
-
-export default function GalleryUplaod() {
-  return (
-  <GalleryUploadComponent/>
-  )
-}
