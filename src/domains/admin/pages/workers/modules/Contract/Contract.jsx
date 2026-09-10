8afc3d438e3bd9ad1f76866b0ab09cae10f7623f
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:21:49 +0300

    chore(cleanup): remove EMS worker module pages

--- src/domains/admin/pages/workers/modules/Contract/Contract.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ContractComponent from './../../../../components/workers/modules/Contract/Contract';
-
-export default function Contract() {
-  return (
-   <ContractComponent/>
-  )
-}
