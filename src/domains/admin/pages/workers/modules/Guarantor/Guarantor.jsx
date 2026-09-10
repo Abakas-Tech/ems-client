498bd452a90670e326f901aec18c62520fdbe976
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:21:51 +0300

    chore(cleanup): remove EMS worker module pages

--- src/domains/admin/pages/workers/modules/Guarantor/Guarantor.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import GuarantorComponent from "./../../../../components/workers/modules/Guarantor/Guarantor";
-
-export default function Guarantor() {
-  return <GuarantorComponent />;
-}
