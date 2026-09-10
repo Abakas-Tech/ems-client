d2c502b75acddb8df89975b59efcfd89eea8cb01
Feysel-A <145969118+Feysel-A@users.noreply.github.com>
2026-09-10 10:21:47 +0300

    chore(cleanup): remove EMS worker module pages

--- src/domains/admin/pages/workers/modules/Travel/Travel.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TravelComponent from './../../../../components/workers/modules/Travel/Travel';
-
-export default function Travel() {
-  return (
-  <TravelComponent/>
-  )
-}
