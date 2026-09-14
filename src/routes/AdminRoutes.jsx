476d3b4dabea4363eafbd469d107f23a8df69b56
kasimnurlgn <kasimnurlgn9@gmail.com>
2026-09-14 08:05:56 +0300

    remove unused analytics code from codebase

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Finances from "../domains/admin/pages/FinancePage/FinancePage.jsx";
 import InvoicesPage from "../domains/admin/pages/Invoices/Invoice.jsx";
-import Analytics from "../domains/admin/pages/AnalyticsPage/AnalyticsPage.jsx";
 import WorkerDashboard from "../domains/admin/pages/workers/WorkerDashboard/WorkerDashboard.jsx";
 import ActiveWorkers from "../domains/admin/pages/workers/ActiveWorkers/ActiveWorkers.jsx";
 import WorkerAutoFill from "../domains/admin/pages/workers/WorkerAutoFill/WorkerAutoFill.jsx";
